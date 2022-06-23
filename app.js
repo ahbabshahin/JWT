2fac13ccbea7f0a05bcec84066879e622d91ebf6
Ahbab <ahbabshahin7788@gmail.com>
2022-06-23 20:50:58 +0600

    Not important

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const app = express();
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-
+const mainRouter = require('./routes/main');
 // middleware
 app.use(express.static('./public'));
 app.use(express.json());
+
+app.use('/api/v1', mainRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -17,13 +19,13 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		app.listen(port, () =>
+			console.log(`Server is listening on port ${port}...`)
+		);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 start();
