a5dc256f6692a801296f82354dd11e0bde92aa61
Ahbab <ahbabshahin7788@gmail.com>
2022-06-23 23:11:25 +0600

    auth middleware added

--- controllers/main.js
+++ controllers/main.js
@@ -20,30 +20,15 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-	const authHeader = req.headers.authorization;
+	// console.log(req.user);
+	const { username } = req.user;
 
-	if (!authHeader || !authHeader.startsWith('Barer ')) {
-		throw new CustomAPIError('No token provided', 401);
-	}
+	const luckyNumber = Math.floor(Math.random() * 100);
 
-	const token = authHeader.split(' ')[1];
-
-	// console.log(token);
-
-	try {
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-		const luckyNumber = Math.floor(Math.random() * 100);
-
-		res.status(200).json({
-			msg: `Hello, ${decoded.username}`,
-			secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-		});
-
-		// console.log(decoded.username);
-	} catch (error) {
-		throw new CustomAPIError('Not  authorized to access this route', 401);
-	}
+	res.status(200).json({
+		msg: `Hello, ${username}`,
+		secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+	});
 };
 
 module.exports = { login, dashboard };
